8a4002a72cabfaa3ed3382672b072aa32f1147a6
Ilya Makarov <makarovilya944@yandex.ru>
2021-03-04 00:35:15 +0700

    Remove shet

--- game/main.py
+++ game/main.py
@@ -1,12 +1,31 @@
-from builder import create_complex
-from exporter import RedisMapMonitor
-from time import sleep
+from random import randint
+from builder import create_random
+from paint import paint_all
+from entities import Map
 
-my_map = create_complex(width=20, height=8)
+# my_map = create_random(width=20, height=8)
 # paint_all(my_map)
 
-monitor = RedisMapMonitor(my_map)
-monitor.export_map()
 
-while True:
-    sleep(1000)
+def one_day(cur_day: Map) -> Map:
+    # Цикл насыщения
+
+    # Цикл кормежки
+
+    # Цикл трат
+
+    # Цикл размножения
+
+    # Цикл вымирания
+    pass
+
+if __name__ == '__main__':
+    start_map = create_random(width=20, height=8)
+    start_plant_count = 8
+    plant_positions = []
+    for i in range(start_plant_count):
+        plant_positions.append((randint(0, 20),
+                               randint(0, 8)))
+
+
+    print('hello')
